7cbfd38ad70d0a4dabdf58bd810a0234e020604d
VanshAdhlakha <vanshadhlakha@Vansh-MacBook-Pro.local>
2025-12-06 10:57:21 +0530

    final

--- pages/JavaScript_files/script.js
+++ pages/JavaScript_files/script.js
@@ -1,48 +1,3 @@
-// let m = document.getElementById('minus');
-// let p = document.getElementById('plus');
-
-
-// if (m && p) {
-//     let q = document.getElementById('qty');
-//     let t = document.getElementById('total');
-
-//     let qnt = 1;      
-//     let price = 500; 
-
-
-//     p.addEventListener('click', function(){
-//         qnt++;                   
-//         q.textContent = qnt;     
-//         t.textContent = price * qnt;
-//     });
-
-
-//     m.addEventListener('click', function(){
-//         if(qnt > 1){             
-//             qnt--;               
-//             q.textContent = qnt; 
-//             t.textContent = price * qnt; 
-//         }
-//     });
-// }
-
-// let magicBox = document.querySelector('#magic-box');
-
-// if (magicBox) {
-
-//     magicBox.addEventListener('mouseover', function(){
-//         magicBox.style.backgroundColor = '#e63946'; 
-//         magicBox.style.transition = "0.3s";        
-//     });
-
-
-//     magicBox.addEventListener('mouseout', function(){
-//         magicBox.style.backgroundColor = '#2c3e3c'; 
-//     });
-// }
-
-
-
 
 const cards = document.querySelectorAll('.item-card');
 
@@ -82,7 +37,7 @@ let magicBox = document.querySelector('#magic-box');
 
 if (magicBox) {
     magicBox.addEventListener('mouseover', function(){
-        magicBox.style.backgroundColor = '#e63946'; 
+        magicBox.style.backgroundColor = '#2c4e3c'; 
         magicBox.style.transition = "0.3s";         
     });
 
